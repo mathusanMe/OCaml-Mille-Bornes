6eaeaeaca4858e9a0a27543749b4e5683e01d190
mathusanMe <mathusan.selvakumar+github@gmail.com>
2023-11-01 11:14:44 +0100

    Add get_current_team_from to board_engine

--- lib/board_engine.mli
+++ lib/board_engine.mli
@@ -8,4 +8,7 @@ type board = {
   current_team_index : int;
 }
 
+val get_current_team_from : board -> team
 val draw_card : board -> team -> board
+(* [draw_card b t] draws a card from the draw pile and adds it to the hand of
+ * the current player on team [t]. If the draw pile is empty, raise EmptyPile. *)
