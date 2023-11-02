4dc827095dd50317acae331fc04bf038997be37f
mathusanMe <89030828+mathusanMe@users.noreply.github.com>
2023-11-02 22:41:11 +0100

    Revert "Added draw_card function to board_engine"
    This reverts commit 4dc74bd522e770cdda2ee34352515fd5768f2815.

--- lib/board_engine.ml
+++ lib/board_engine.ml
@@ -7,14 +7,3 @@ type board = {
   teams : team list;
   current_team_index : int;
 }
-
-let draw_card (b : board) (t : team) =
-  let card, new_draw_pile = draw_card_from_pile b.draw_pile in
-  let player = get_current_player_from t in
-  let player_struct = get_player_struct_from player in
-  let new_hand = sort_card_list (card :: player_struct.hand) in
-  let new_player_struct = { player_struct with hand = new_hand } in
-  let new_player = replace_player_struct_in player new_player_struct in
-  let new_team = replace_player_in t new_player in
-  let new_teams = replace_team_in b.teams new_team in
-  { b with draw_pile = new_draw_pile; teams = new_teams }
