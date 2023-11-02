55d4ab6c29a2b6195c37e2425bbad0154efc48b4
mathusanMe <89030828+mathusanMe@users.noreply.github.com>
2023-11-02 22:41:11 +0100

    Revert "Added auxiliary functions to teams_engine"
    This reverts commit c49973823822a1b68a4be9decefafa06d1856cf5.

--- lib/teams_engine.mli
+++ lib/teams_engine.mli
@@ -23,11 +23,6 @@ val get_current_player_from : team -> player
 val get_player_struct_from : player -> player_struct
 val set_next_player_from : team -> team
 val is_computer : player -> bool
-val same_player : player -> player -> bool
-val same_team : team -> team -> bool
-val replace_player_struct_in : player -> player_struct -> player
-val replace_player_in : team -> player -> team
-val replace_team_in : team list -> team -> team list
 val init_team_with_one_player : string -> bool -> team
 val init_team_with_two_players : string -> bool -> string -> bool -> team
 val has_already_used_safety_card : team -> safety_card -> bool
